ae825b924352752f45e3f88e2b0afbd637051d58
Vybhavreddy K C <vybhav19@gmail.com>
2020-01-21 13:35:12 -0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -14,6 +14,8 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
+defaults.device = torch.device('cpu')
+
 export_file_url = 'https://drive.google.com/uc?export=download&id=1GkBc9d3OkzNRvga0xDwURPHKPtp2QKfZ'
 export_file_name = 'export.pkl'
 
